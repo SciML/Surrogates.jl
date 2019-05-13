f6b9d5e146ac6998ac2fb987865785d428fe4ddc
ludoro <ludovicobessi@gmail.com>
2019-05-13 16:30:27 +0200

    Added file Radials_1D containing methods for polynomial and radial basis function interpolation

---
+++ src/Radials_1D.jl
@@ -0,0 +1,110 @@
+function Radial_1D(x,y,a,b,kind::String,lambda = 0)
+    #=
+    (x,y) set of nodes
+    (a,b) interval
+    kind is type of radial basis function
+    lambda is optional parameter with kind == multiquadric
+    =#
+
+
+    #1D Chebyshev is suggested
+    Chebyshev(x,k) = cos(k*acos(-1 + 2/(b-a)*(x-a)))
+
+    #Type of Radial basis function
+    #q is the number of polynomials in the basis
+    #The numbers are suggested by papers
+    if lambda === nothing
+        if kind == "linear"
+            q = 1
+            phi = z -> abs(z)
+        elseif kind == "cubic"
+            q = 2
+            phi = z -> abs(z)^3
+        elseif kind == "thinplate"
+            q = 2
+            phi = z -> z^2*log(abs(z))
+        else
+            error("Wrong type")
+        end
+    else
+        if kind == "multiquadric"
+            q = 1
+            phi = z -> sqrt(abs(z)^2 + lambda^2)
+        else
+            error("Wrong type")
+        end
+    end
+    if length(x) != length(y)
+        error("Data length does not match")
+    end
+    n = length(x)
+    #Find coefficients for both radial basis functions and polynomial terms
+    size = n+q
+    D = zeros(Float32, size, size)
+    d = zeros(Float32,size)
+    #In this array I have in the first n entries the coefficient of the radial
+    #basis function, in the last q term the coefficients for polynomial terms
+    coeff = zeros(Float32,size)
+
+    #Matrix made by 4 blocks:
+    #=
+    A | B
+    B^t | 0
+    A nxn, B nxq A,B symmetric so the matrix D is symmetric as well.
+    =#
+
+
+    for i = 1:n
+        d[i] =  y[i]
+        for j = 1:n
+            D[i,j] = phi(x[i] - x[j])
+        end
+        for k = n+1:size
+            #Symmetric
+            D[i,k] = Chebyshev(x[i],k)
+            D[k,i] = D[i,k]
+        end
+    end
+
+    #Vector of size n + q containing in the first n terms the coefficients of
+    # the radial basis function and in the last q term the coefficient for the
+    # polynomials
+    return D\d
+
+end
+
+function evaluate(value,coeff,x,a,b,kind::String,lambda = 0)
+
+    Chebyshev(x,k) = cos(k*acos(-1 + 2/(b-a)*(x-a)))
+    if lambda === nothing
+        if kind == "linear"
+            q = 1
+            phi = z -> abs(z)
+        elseif kind == "cubic"
+            q = 2
+            phi = z -> abs(z)^3
+        elseif kind == "thinplate"
+            q = 2
+            phi = z -> z^2*log(abs(z))
+        else
+            error("Wrong type")
+        end
+    else
+        if kind == "multiquadric"
+            q = 1
+            phi = z -> sqrt(abs(z)^2 + lambda^2)
+        else
+            error("Wrong type")
+        end
+    end
+    n = length(x)
+    q = length(coeff) - n
+    approx = 0
+    for i = 1:n
+        approx = approx + coeff[i]*phi(value - x[i])
+    end
+    for i = n+1:q
+        approx = approx + coeff[i]*Chebyshev(value,n+1-i)
+    end
+    return approx
+end
