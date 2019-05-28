0a3d1ea722a7feb002d908b8da0f0c4557133331
ludoro <ludovicobessi@gmail.com>
2019-05-28 22:24:34 +0200

    Changed functions Radials_1D and Krigin_1D to account for new abstract type, missing tests that will be available tomorrow

--- src/Kriging_1D.jl
+++ src/Kriging_1D.jl
@@ -1,17 +1,15 @@
 """
-theta_l > 0 higher theta models functions that are highly changing in the
-            l-th coordinate,
-0 < p <= 2, higher p -> function being modelled is smoother
+Kriging:
+(x,y): sampled point
+p: value between 0 and 2 modelling the
+   smoothness of the function being approximated, 0-> rough  2-> C^infinity
 """
 function Kriging_1D(x,y,p)
-
     if length(x) != length(y)
         error("Dimension of x and y are not equal")
     end
-    theta_l = 1
     n = length(x)
-
-    #Covariance matrix R
+    theta_l = 1
     R = zeros(Float32, n, n)
     for i = 1:n
         for j = 1:n
@@ -32,17 +30,24 @@ end
 """
 Kriging predictor:
 y(x*) = mu + sum(b_i * phi(x* - x[i]))
+new_point: value at which we want the approximation
+x: set of points
+p: value between 0 and 2 modelling the
+   smoothness of the function being approximated, 0-> rough  2-> C^infinity
+mu,b,sigma,inv values returned from Krigin_1D
+
+Returns the prediction at the new point and the expected mean squared error at
+that point
 """
-function evaluate(new_point,p,mu,b,x,sigma,inv)
+function evaluate(new_point,x,p,mu,b,sigma,inv)
     n = length(x)
-    #Building the predictor
     phi(z) = exp(-(abs(z))^p)
     prediction = 0
     for i = 1:n
         prediction = prediction + b[i]*phi(new_point-x[i])
     end
     prediction = mu + prediction
-    #Building the error at the new point
+
     r = zeros(Float32,n,1)
     for i = 1:n
         r[i] = phi(new_point - x[i])
@@ -55,6 +60,5 @@ function evaluate(new_point,p,mu,b,x,sigma,inv)
     b = b[1]
     mean_squared_error = sigma*(1 - a + (1-a)^2/(b))
     std_error = sqrt(mean_squared_error)
-
     return prediction,std_error
 end
--- src/Radials_1D.jl
+++ src/Radials_1D.jl
@@ -1,3 +1,5 @@
+using LinearAlgebra
+
 abstract type AbstractBasisFunction end
 
 struct Basis <: AbstractBasisFunction
@@ -14,43 +16,19 @@ end
 
 
 """
-(x,y) set of nodes
-(a,b) interval
-kind is type of radial basis function
-lambda is optional parameter with kind == multiquadric
+(x,y): set of nodes
+(a,b): interval
+basisFunc: selected Basis function
+Output: vector of coefficients n+q required by the estimator
 """
-function Radial_1D(x,y,a,b,kind::String,lambda = 0)
+function Radial_1D(x,y,a,b,basisFunc::AbstractBasisFunction)
     if length(x) != length(y)
         error("Data length does not match")
     end
-
     Chebyshev(x,k) = cos(k*acos(-1 + 2/(b-a)*(x-a)))
 
-    #Type of Radial basis function
-    #q is the number of polynomials in the basis
-    #The numbers are suggested by papers
-    if lambda === nothing
-        if kind == "linear"
-            q = 1
-            phi = z -> abs(z)
-        elseif kind == "cubic"
-            q = 2
-            phi = z -> abs(z)^3
-        elseif kind == "thinplate"
-            q = 2
-            phi = z -> z^2*log(abs(z))
-        else
-            error("Wrong type")
-        end
-    else
-        if kind == "multiquadric"
-            q = 1
-            phi = z -> sqrt(abs(z)^2 + lambda^2)
-        else
-            error("Wrong type")
-        end
-    end
     n = length(x)
+    q = basisFunc.number_of_elements_in_polynomial_basis
     #Find coefficients for both radial basis functions and polynomial terms
     size = n+q
     D = zeros(float(eltype(x)), size, size)
@@ -69,7 +47,7 @@ function Radial_1D(x,y,a,b,kind::String,lambda = 0)
     @inbounds for i = 1:n
         d[i] =  y[i]
         for j = 1:n
-            D[i,j] = phi(x[i] - x[j])
+            D[i,j] = basisFunc.phi(x[i] - x[j])
         end
         if i < n + 1
             for k = n+1:size
@@ -80,42 +58,24 @@ function Radial_1D(x,y,a,b,kind::String,lambda = 0)
 
     Sym = Symmetric(D,:U)
 
-    #Vector of size n + q containing in the first n terms the coefficients of
-    # the radial basis function and in the last q term the coefficient for the
-    # polynomials
     return Sym\d
 
 end
 
-function evaluate(value,coeff,x,a,b,kind::String,lambda = 0)
+"""
+value: value at which you want the approximation
+coeff: returned vector from Radial_1D
+x: set of points
+(a,b): interval
+"""
+function evaluate(value,coeff,x,a,b,basisFunc::AbstractBasisFunction)
 
     Chebyshev(x,k) = cos(k*acos(-1 + 2/(b-a)*(x-a)))
-    if lambda === nothing
-        if kind == "linear"
-            q = 1
-            phi = z -> abs(z)
-        elseif kind == "cubic"
-            q = 2
-            phi = z -> abs(z)^3
-        elseif kind == "thinplate"
-            q = 2
-            phi = z -> z^2*log(abs(z))
-        else
-            error("Wrong type")
-        end
-    else
-        if kind == "multiquadric"
-            q = 1
-            phi = z -> sqrt(abs(z)^2 + lambda^2)
-        else
-            error("Wrong type")
-        end
-    end
     n = length(x)
-    q = length(coeff) - n
+    q = basisFunc.number_of_elements_in_polynomial_basis
     approx = 0
     for i = 1:n
-        approx = approx + coeff[i]*phi(value - x[i])
+        approx = approx + coeff[i]*basisFunc.phi(value - x[i])
     end
     for i = n+1:q
         approx = approx + coeff[i]*Chebyshev(value,n+1-i)
