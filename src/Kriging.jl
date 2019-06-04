45d5ab87cb790052e3795a8bc0a9f647e084cc51
ludoro <ludovicobessi@gmail.com>
2019-06-04 22:55:04 +0200

    Added functions add_point and current_estimate

--- src/Kriging.jl
+++ src/Kriging.jl
@@ -13,6 +13,7 @@ struct Kriging <: AbstractBasisFunction
     x
     y
     p
+    theta
     mu
     b
     sigma
@@ -37,11 +38,11 @@ Constructor for type Kriging.
 """
 function Kriging(new_point::Number,x::Array,y::Array,p::Number)
     n = length(x)
-    theta_l = 1
+    theta = 1
     R = zeros(float(eltype(x)), n, n)
     @inbounds for i = 1:n
         for j = 1:n
-            R[i,j] = exp(-theta_l*abs(x[i]-x[j])^p)
+            R[i,j] = exp(-theta*abs(x[i]-x[j])^p)
         end
     end
     one = ones(n,1)
@@ -71,7 +72,7 @@ function Kriging(new_point::Number,x::Array,y::Array,p::Number)
     b = b[1]
     mean_squared_error = sigma[1]*(1 - a + (1-a)^2/(b))
     std_error = sqrt(mean_squared_error)
-    Kriging(x,y,p,mu,b,sigma,inverse_of_R,new_point,prediction,std_error)
+    Kriging(x,y,p,theta,mu,b,sigma,inverse_of_R,new_point,prediction,std_error)
 end
 
 
@@ -137,5 +138,75 @@ function Kriging(new_point::Array,x::Array,y::Array,p::Array,theta::Array)
     b = b[1]
     mean_squared_error = sigma[1]*(1 - a + (1-a)^2/(b))
     std_error = sqrt(mean_squared_error)
-    Kriging(x,y,p,mu,b,sigma,inverse_of_R,new_point,prediction,std_error)
+    Kriging(x,y,p,theta,mu,b,sigma,inverse_of_R,new_point,prediction,std_error)
+end
+
+
+"""
+    add_point!(k::AbstractBasisFunction,new_x::Array,new_y::Array)
+
+Adds the new point and its respective value to the sample points.
+Returns the updated Kriging model.
+
+"""
+function add_point!(k::AbstractBasisFunction,new_x::Array,new_y::Array)
+    k.x = vcat(k.x,new_x)
+    k.y = vcat(k.y,new_y)
+    return Kriging(k.x,k.y,k.p,k.theta,k.mu,k.b,k.sigma,k.inverse_of_R,k.new_point,k.prediction,k.std_error)
+end
+
+
+"""
+    current_estimate(k::AbstractBasisFunction,val::Array)
+
+Gives the current estimate for array value 'val' with respect to the Kriging object k.
+"""
+function current_estimate(k::AbstractBasisFunction,val::Array)
+    prediction = zero(eltype(x))
+    r = zeros(float(eltype(x)),n,1)
+    @inbounds for i = 1:n
+        sum = zero(eltype(x))
+        for l = 1:d
+            sum = sum + k.theta[l]*norm(val[l]-k.x[i,l])^k.p[l]
+        end
+        r[i] = exp(-sum)
+        prediction = prediction + k.b[i]*exp(-sum)
+    end
+    prediction = k.mu + prediction
+
+    one = ones(n,1)
+    one_t = one'
+    a = r'*k.inverse_of_R*r
+    a = a[1]
+    b = one_t*k.inverse_of_R*one
+    b = b[1]
+    mean_squared_error = k.sigma*(1 - a + (1-a)^2/(b))
+    std_error = sqrt(mean_squared_error)
+    return prediction, std_error
+end
+
+"""
+    current_estimate(k::AbstractBasisFunction,val::Array)
+
+Gives the current estimate for 'val' with respect to the Kriging object k.
+"""
+function current_estimate(k::AbstractBasisFunction,val::Number)
+    phi(z) = exp(-(abs(z))^p)
+    prediction = zero(eltype(x))
+    r = zeros(float(eltype(x)),n,1)
+    @inbounds for i = 1:n
+        prediction = prediction + k.b[i]*phi(val-k.x[i])
+        r[i] = phi(new_point - k.x[i])
+    end
+    prediction = k.mu + prediction
+
+    one = ones(n,1)
+    one_t = one'
+    a = r'*k.inverse_of_R*r
+    a = a[1]
+    b = one_t*k.inverse_of_R*one
+    b = b[1]
+    mean_squared_error = k.sigma*(1 - a + (1-a)^2/(b))
+    std_error = sqrt(mean_squared_error)
+    return prediction, std_error
 end
