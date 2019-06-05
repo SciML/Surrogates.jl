81cb2552b7bdd0fc2ace53679c69495698b9694d
ludoro <ludovicobessi@gmail.com>
2019-06-05 23:13:51 +0200

    Finished work on refactoring Kriging

--- src/Kriging.jl
+++ src/Kriging.jl
@@ -9,7 +9,7 @@ abstract type AbstractBasisFunction end
 
 export Kriging
 
-struct Kriging <: AbstractBasisFunction
+mutable struct Kriging <: AbstractBasisFunction
     x
     y
     p
@@ -18,25 +18,21 @@ struct Kriging <: AbstractBasisFunction
     b
     sigma
     inverse_of_R
-    new_point
-    prediction
-    std_error
  end
 
 
 """
-    Kriging(new_point::Number,x::Array,y::Array,p::Number)
+    Kriging(x::Array,y::Array,p::Number)
 
 Constructor for type Kriging.
 
 #Arguments:
--'new_point': value at which you want to calculate approximation
 -(x,y): sampled points
 -'p': value between 0 and 2 modelling the
    smoothness of the function being approximated, 0-> rough  2-> C^infinity
 
 """
-function Kriging(new_point::Number,x::Array,y::Array,p::Number)
+function Kriging(x::Array,y::Array,p::Number)
     n = length(x)
     theta = 1
     R = zeros(float(eltype(x)), n, n)
@@ -52,38 +48,17 @@ function Kriging(new_point::Number,x::Array,y::Array,p::Number)
     b = inverse_of_R*(y-one*mu)
     sigma = ((y-one*mu)' * inverse_of_R * (y - one*mu))/n
     #mu[1], b, sigma[1],inverse_of_R
-
-    phi(z) = exp(-(abs(z))^p)
-    prediction = zero(eltype(x))
-    @inbounds for i = 1:n
-        prediction = prediction + b[i]*phi(new_point-x[i])
-    end
-    prediction = mu[1] + prediction
-
-    r = zeros(float(eltype(x)),n,1)
-    @inbounds for i = 1:n
-        r[i] = phi(new_point - x[i])
-    end
-    one = ones(n,1)
-    one_t = one'
-    a = r'*inverse_of_R*r
-    a = a[1]
-    b = one_t*inverse_of_R*one
-    b = b[1]
-    mean_squared_error = sigma[1]*(1 - a + (1-a)^2/(b))
-    std_error = sqrt(mean_squared_error)
-    Kriging(x,y,p,theta,mu,b,sigma,inverse_of_R,new_point,prediction,std_error)
+    Kriging(x,y,p,theta,mu[1],b,sigma[1],inverse_of_R)
 end
 
 
 
 """
-    Kriging(new_point::Array,x::Array,y::Array,p::Array,theta::Array)
+    Kriging(x::Array,y::Array,p::Array,theta::Array)
 
 Constructor for type Kriging.
 
 #Arguments:
--'new_point': Array at which you want to calculate approximation
 -''(x,y)': sampled points
 -'p': array of values between 0 and 2 modelling the
    smoothness of the function being approximated in the i-th variable,
@@ -92,7 +67,7 @@ Constructor for type Kriging.
           changing in the i-th variable
 
 """
-function Kriging(new_point::Array,x::Array,y::Array,p::Array,theta::Array)
+function Kriging(x::Array,y::Array,p::Array,theta::Array)
     n = size(x,1)
     d = size(x,2)
     R = zeros(float(eltype(x)), n, n)
@@ -112,33 +87,7 @@ function Kriging(new_point::Array,x::Array,y::Array,p::Array,theta::Array)
     b = inverse_of_R*(y-one*mu)
     sigma = ((y-one*mu)' * inverse_of_R * (y - one*mu))/n
     #mu[1], b, sigma[1],inverse_of_R
-    prediction = zero(eltype(x))
-    @inbounds for i = 1:n
-        sum = zero(eltype(x))
-        for l = 1:d
-            sum = sum + theta[l]*norm(new_point[l]-x[i,l])^p[l]
-        end
-        prediction = prediction + b[i]*exp(-sum)
-    end
-    prediction = mu[1] + prediction
-
-    r = zeros(float(eltype(x)),n,1)
-    @inbounds for i = 1:n
-        sum = zero(eltype(x))
-        for l = 1:d
-            sum = sum + theta[l]*norm(new_point[l]-x[i,l])^p[l]
-        end
-        r[i] = exp(-sum)
-    end
-    one = ones(n,1)
-    one_t = one'
-    a = r'*inverse_of_R*r
-    a = a[1]
-    b = one_t*inverse_of_R*one
-    b = b[1]
-    mean_squared_error = sigma[1]*(1 - a + (1-a)^2/(b))
-    std_error = sqrt(mean_squared_error)
-    Kriging(x,y,p,theta,mu,b,sigma,inverse_of_R,new_point,prediction,std_error)
+    Kriging(x,y,p,theta,mu[1],b,sigma[1],inverse_of_R)
 end
 
 
@@ -146,23 +95,26 @@ end
     add_point!(k::AbstractBasisFunction,new_x::Array,new_y::Array)
 
 Adds the new point and its respective value to the sample points.
+Warning: If you are just adding a single point, you have to wrap it with []
 Returns the updated Kriging model.
 
 """
 function add_point!(k::AbstractBasisFunction,new_x::Array,new_y::Array)
     k.x = vcat(k.x,new_x)
     k.y = vcat(k.y,new_y)
-    return Kriging(k.x,k.y,k.p,k.theta,k.mu,k.b,k.sigma,k.inverse_of_R,k.new_point,k.prediction,k.std_error)
+    return Kriging(k.x,k.y,k.p,k.theta,k.mu,k.b,k.sigma,k.inverse_of_R)
 end
 
 
 """
     current_estimate(k::AbstractBasisFunction,val::Array)
 
-Gives the current estimate for array value 'val' with respect to the Kriging object k.
+Gives the current estimate for array 'val' with respect to the Kriging object k.
 """
 function current_estimate(k::AbstractBasisFunction,val::Array)
     prediction = zero(eltype(x))
+    n = Base.size(x,1)
+    d = Base.size(x,2)
     r = zeros(float(eltype(x)),n,1)
     @inbounds for i = 1:n
         sum = zero(eltype(x))
@@ -192,11 +144,12 @@ Gives the current estimate for 'val' with respect to the Kriging object k.
 """
 function current_estimate(k::AbstractBasisFunction,val::Number)
     phi(z) = exp(-(abs(z))^p)
+    n = length(x)
     prediction = zero(eltype(x))
     r = zeros(float(eltype(x)),n,1)
     @inbounds for i = 1:n
         prediction = prediction + k.b[i]*phi(val-k.x[i])
-        r[i] = phi(new_point - k.x[i])
+        r[i] = phi(val - k.x[i])
     end
     prediction = k.mu + prediction
 
