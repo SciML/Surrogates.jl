cc2e7c74ccdf8682c9f813f0f183e948153130d5
ludoro <ludovicobessi@gmail.com>
2019-06-12 22:23:13 +0200

    First barebone implementation of Random sampling, uniform sampling, Sobol and LHS

---
+++ src/Sampling.jl
@@ -0,0 +1,53 @@
+#=
+1) Random sampling
+2) Uniform sampling
+3) Sobol sampling
+4) Latin Hypercube
+5) Sparse grid sampling
+6) Monte carlo importance sampling
+7) Low- discrepancy sampling
+=#
+using LinearAlgebra
+using Distributions
+
+"""
+sample(f,kind_of_sample()) returns the evaluation of function f
+and the sampling points, ready to be used in Kriging and Radials.
+"""
+function sample(f::Function,sample)
+    n = Base.size(sample,1)
+    vals = zeros(eltype(sample),n,1)
+    for i = 1:n
+        vals[i] = f(sample[i,:])
+    end
+    return vals,sample
+end
+
+"""
+random_sample(n,d,bounds) returns a nxd Array containing
+random numbers
+"""
+function random_sample(n,d,bounds)
+    return rand(bounds[1]:0.0000001:bounds[2],n,d)
+end
+
+"""
+uniform_sample
+"""
+function uniform_sample(n,d,bounds)
+    return rand(Uniform(bounds[1],bounds[2]),n,d)
+end
+
+"""
+Sobol
+"""
+function sobol_sample(lb,ub)
+    return SobolSeq(lb,ub)
+end
+
+"""
+Latin hypercube sapling
+"""
+function LHS_sample(n,d)
+    return LHCoptim(n,d,1)[1]
+end
