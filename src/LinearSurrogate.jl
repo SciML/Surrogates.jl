18ec17141a3ea770c2cc08a4c2cd8da96c55072a
ludoro <ludovicobessi@gmail.com>
2019-07-17 09:56:27 +0200

    1D Linear

--- src/LinearSurrogate.jl
+++ src/LinearSurrogate.jl
@@ -2,14 +2,13 @@ mutable struct LinearSurrogate{X,Y,C,L,U} <: AbstractSurrogate
     x::X
     y::Y
     coeff::C
-    L::L
-    U::U
-
+    lb::L
+    ub::U
 end
 
 
 function (lin::LinearSurrogate)(val::Number)
-    return coef(lin)[1] + val*coef(lin)[2]
+    return lin.coeff[1] + val*lin.coeff[2]
 end
 
 function LinearSurrogate(x,y,lb::Number,ub::Number)
@@ -18,16 +17,35 @@ function LinearSurrogate(x,y,lb::Number,ub::Number)
     LinearSurrogate(x,y,coef(ols),lb,ub)
 end
 
+function add_point!(my_linear::LinearSurrogate,new_x,new_y)
+    if (length(new_x) == 1 && length(new_x[1]) == 1) || ( length(new_x) > 1 && length(new_x[1]) == 1 && length(my_linear.ub[1])>1)
+        push!(my_linear.x,new_x)
+        push!(my_linear.y,new_y)
+        if length(my_linear.ub[1]) == 1
+            df = DataFrame(X=my_linear.x,Y=my_linear.y)
+            md = lm(@formula(Y ~ X), df)
+            my_linear.coeff = coef(md)
+        else
+            #TODO
+        end
+    else
+        append!(my_linear.x,new_x)
+        append!(my_linear.y,new_y)
+        if length(my_linear.lb[1]) == 1
+            df = DataFrame(X=my_linear.x,Y=my_linear.y)
+            md = lm(@formula(Y ~ X), df)
+            my_linear.coeff = coef(md)
+        else
+            #TODO
+        end
+    end
+    nothing
+end
+
 function (lin::LinearSurrogate)(val)
-    return coef(lin)[1] + val*coef[lin][2]
+
 end
 
 function LinearSurrogate(x,y,lb,ub)
-    DF = ??
-    ols = ??
-    LinearSurrogate(x,y,coef(ols),lb,ub)
-end
-
-function add_point!(my_linear::LinearSurrogate,x_new,y_new)
 
 end
--- test/linearSurrogate.jl
+++ test/linearSurrogate.jl
@@ -1,1 +1,13 @@
 using Surrogates
+
+#1D
+x = [1.0,2.0,3.0]
+y = [1.5,3.5,5.3]
+lb = 0.0
+ub = 7.0
+my_linear_surr = LinearSurrogate(x,y,lb,ub)
+val = my_linear_surr(5.0)
+add_point!(my_linear_surr,4.0,7.2)
+add_point!(my_linear_surr,[5.0,6.0],[8.3,9.7])
+
+#ND
