ca170cfdb63133649080ffa7904b1bad8a43e0aa
ludoro <ludovicobessi@gmail.com>
2019-07-18 21:12:56 +0200

    Loba ND done

--- src/Lobachesky.jl
+++ src/Lobachesky.jl
@@ -38,6 +38,9 @@ end
 Lobachesky interpolation, suggested parameters: 0 <= alpha <= 4, n must be even.
 """
 function LobacheskySurrogate(x,y,alpha,n::Int,lb::Number,ub::Number)
+    if alpha > 4 || alpha < 0
+        error("Alpha must be between 0 and 4")
+    end
     if n % 2 != 0
         error("Parameter n must be even")
     end
@@ -53,14 +56,65 @@ function (loba::LobacheskySurrogate)(val::Number)
     return res
 end
 
+function phi_njND(point,x,alpha,n)
+    s = 1.0
+    d = length(x)
+    for h = 1:d
+        a = phi_nj1D(point[h],x[h],alpha,n)
+        s = s*a
+    end
+    return s
+end
+
+function _calc_loba_coeffND(x,y,alpha,n)
+    dim = length(x)
+    D = zeros(eltype(x[1]), dim, dim)
+    for i = 1:dim
+        for j = 1:dim
+            D[i,j] = phi_njND(x[i],x[j],alpha,n)
+        end
+    end
+    Sym = Symmetric(D,:U)
+    return Sym\y
+end
+
+function LobacheskySurrogate(x,y,alpha,n::Int,lb,ub)
+    if alpha > 4 || alpha < 0
+        error("Alpha must be between 0 and 4")
+    end
+    if n % 2 != 0
+        error("Parameter n must be even")
+    end
+    coeff = _calc_loba_coeffND(x,y,alpha,n)
+    LobacheskySurrogate(x,y,alpha,n,lb,ub,coeff)
+end
+
+function (loba::LobacheskySurrogate)(val)
+    val = collect(val)
+    res = zero(eltype(loba.y[1]))
+    for j = 1:length(loba.x)
+        res = res + loba.coeff[j]*phi_njND(val,loba.x[j],loba.alpha,loba.n)
+    end
+    return res
+end
+
 function add_point!(loba::LobacheskySurrogate,x_new,y_new)
     if length(loba.x[1]) == 1
+        #1D
         append!(loba.x,x_new)
         append!(loba.y,y_new)
         loba.coeff = _calc_loba_coeff1D(loba.x,loba.y,loba.alpha,loba.n)
-        #1D
     else
         #ND
-        #TODO
+        if length(x_new[1]) == 1
+            push!(loba.x,x_new)
+            push!(loba.y,y_new)
+            loba.coeff = _calc_loba_coeffND(loba.x,loba.y,loba.alpha,loba.n)
+        else
+            loba.x = vcat(loba.x,x_new)
+            loba.y = vcat(loba.y,y_new)
+            loba.coeff = _calc_loba_coeffND(loba.x,loba.y,loba.alpha,loba.n)
+        end
     end
+    nothing
 end
--- test/lobachesky.jl
+++ test/lobachesky.jl
@@ -1,15 +1,27 @@
 using Surrogates
+using LinearAlgebra
 
 #1D
-obj = x -> 3*x+1
+obj = x -> exp(x)*x+1
 a = 0.0
 b = 4.0
-x = [0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0]
+x = sample(20,a,b,SobolSample())
 y = obj.(x)
 alpha = 2.0
 n = 2
 my_loba = LobacheskySurrogate(x,y,alpha,n,a,b)
-val = my_loba(3.5)
+val = my_loba(3.83)
 add_point!(my_loba,3.7,12.1)
+add_point!(my_loba,[1.23,3.45],[5.20,109.67])
 
 #ND
+obj = x -> 3*norm(x) + 1
+lb = [0.0,0.0]
+ub = [10.0,10.0]
+alpha = 2.0
+n = 2
+x = sample(5,lb,ub,SobolSample())
+y = obj.(x)
+my_loba_ND = LobacheskySurrogate(x,y,alpha,n,lb,ub)
+add_point!(my_loba_ND,[2.0,4.2],4.65188)
+add_point!(my_loba_ND,[[2.0, 3.0],[5.4,3.3]],[2.236,6.328])
