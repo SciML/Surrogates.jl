5f5e5cfa6532e7c1ee8b89d26393d2079470364b
ludoro <ludovicobessi@gmail.com>
2019-07-18 18:58:49 +0200

    Lobachesky surrogate

--- src/Lobachesky.jl
+++ src/Lobachesky.jl
@@ -23,13 +23,7 @@ function phi_nj1D(point,x,alpha,n)
     return val
 end
 
-"""
-Lobachesky interpolation, suggested parameters: 0 <= alpha <= 4, n even.
-"""
-function LobacheskySurrogate(x,y,alpha,n::Int,lb::Number,ub::Number)
-    if n % 2 != 0
-        error("Parameter n must be even")
-    end
+function _calc_loba_coeff1D(x,y,alpha,n)
     dim = length(x)
     D = zeros(eltype(x[1]), dim, dim)
     for i = 1:dim
@@ -38,8 +32,16 @@ function LobacheskySurrogate(x,y,alpha,n::Int,lb::Number,ub::Number)
         end
     end
     Sym = Symmetric(D,:U)
-    coeff = Sym\y
-    #calcolo coeff
+    return Sym\y
+end
+"""
+Lobachesky interpolation, suggested parameters: 0 <= alpha <= 4, n must be even.
+"""
+function LobacheskySurrogate(x,y,alpha,n::Int,lb::Number,ub::Number)
+    if n % 2 != 0
+        error("Parameter n must be even")
+    end
+    coeff = _calc_loba_coeff1D(x,y,alpha,n)
     LobacheskySurrogate(x,y,alpha,n,lb,ub,coeff)
 end
 
@@ -49,4 +51,16 @@ function (loba::LobacheskySurrogate)(val::Number)
         res = res + loba.coeff[j]*phi_nj1D(val,loba.x[j],loba.alpha,loba.n)
     end
     return res
+end
+
+function add_point!(loba::LobacheskySurrogate,x_new,y_new)
+    if length(loba.x[1]) == 1
+        append!(loba.x,x_new)
+        append!(loba.y,y_new)
+        loba.coeff = _calc_loba_coeff1D(loba.x,loba.y,loba.alpha,loba.n)
+        #1D
+    else
+        #ND
+        #TODO
+    end
 end
--- test/lobachesky.jl
+++ test/lobachesky.jl
@@ -1,12 +1,15 @@
 using Surrogates
 
-x = [1.0,2.0,3.0]
-y = [4.0,5.0,6.0]
+#1D
+obj = x -> 3*x+1
 a = 0.0
 b = 4.0
+x = [0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0]
+y = obj.(x)
 alpha = 2.0
 n = 2
 my_loba = LobacheskySurrogate(x,y,alpha,n,a,b)
-println(my_loba.coeff)
-val = my_loba(4.0)
-println(val)
+val = my_loba(3.5)
+add_point!(my_loba,3.7,12.1)
+
+#ND
