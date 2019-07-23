b6ddcf189b117ee0429fe7d13a25d1ec9c6d8af6
ludoro <ludovicobessi@gmail.com>
2019-07-23 15:25:16 +0200

    Random forest surrogate 1D and ND using XGboost library, tested.

--- src/Surrogates.jl
+++ src/Surrogates.jl
@@ -6,6 +6,7 @@ using GLM
 using Distributions
 using Sobol
 using LatinHypercubeSampling
+using XGBoost
 
 abstract type AbstractSurrogate <: Function end
 
@@ -15,9 +16,11 @@ include("Sampling.jl")
 include("Optimization.jl")
 include("Lobachesky.jl")
 include("LinearSurrogate.jl")
+include("RandomForestSurrogate.jl")
 export Kriging, RadialBasis, add_point!, current_estimate, std_error_at_point
 export sample, GridSample, UniformSample, SobolSample, LatinHypercubeSample, LowDiscrepancySample
 export SRBF,LCBS,EI,DYCORS,surrogate_optimize
 export LobacheskySurrogate, lobachesky_integral
 export LinearSurrogate
+export RandomForestSurrogate
 end
---
+++ test/random_forest.jl
@@ -0,0 +1,24 @@
+using Surrogates
+using XGBoost
+
+#1D
+obj_1D = x -> 3*x+1
+x = [1.0,2.0,3.0,4.0,5.0]
+y = obj_1D.(x)
+a = 0.0
+b = 10.0
+my_forest_1D = RandomForestSurrogate(x,y,a,b)
+val = my_forest_1D(3.5)
+add_point!(my_forest_1D,6.0,19.0)
+add_point!(my_forest_1D,[7.0,8.0],obj_1D.([7.0,8.0]))
+
+#ND
+lb = [0.0,0.0]
+ub = [10.0,10.0]
+x = sample(5,lb,ub,SobolSample())
+obj_ND = x -> x[1] * x[2]^2
+y = obj_ND.(x)
+my_forest_ND = RandomForestSurrogate(x,y,lb,ub)
+val = my_forest_ND([1.0 1.0])
+add_point!(my_forest_ND,[1.0 1.0],1.0)
+add_point!(my_forest_ND,[1.2 1.2; 1.5 1.5],[1.728,3.375])
