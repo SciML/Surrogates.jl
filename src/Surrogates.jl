d842b4e3148606e3e4758c97e7d5bf150d7e574b
ludoro <ludovicobessi@gmail.com>
2019-07-24 12:41:03 +0200

    Setup

--- src/Surrogates.jl
+++ src/Surrogates.jl
@@ -15,9 +15,11 @@ include("Sampling.jl")
 include("Optimization.jl")
 include("Lobachesky.jl")
 include("LinearSurrogate.jl")
+include("SVMSurrogate.jl")
 export Kriging, RadialBasis, add_point!, current_estimate, std_error_at_point
 export sample, GridSample, UniformSample, SobolSample, LatinHypercubeSample, LowDiscrepancySample
 export SRBF,LCBS,EI,DYCORS,surrogate_optimize
 export LobacheskySurrogate, lobachesky_integral
 export LinearSurrogate
+export SVMSurrogate
 end
