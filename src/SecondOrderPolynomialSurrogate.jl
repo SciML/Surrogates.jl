49e1dbb295fd9f919a4ecbf890fa6cbf8208d59c
ludoro <ludovicobessi@gmail.com>
2019-08-15 11:16:19 +0200

    Added ND prediction

--- src/SecondOrderPolynomialSurrogate.jl
+++ src/SecondOrderPolynomialSurrogate.jl
@@ -25,12 +25,37 @@ function (sec_ord::SecondOrderPolynomialSurrogate)(val::Number)
 end
 
 function SecondOrderPolynomialSurrogate(x,y,lb,ub)
-    #TODO
+    n = length(x)
+    d = length(lb)
+    X = ones(eltype(x[1]),n,1+d+(d-1)+d)
+    for j = 1:d
+        X[:,j+1] =[x[i][j] for i=1:n]
+    end
+    for j = 1:d-1
+        X[:,j+d+1] = [x[i][j]*x[i][j+1] for i = 1:n]
+    end
+    for j = 1:d
+        X[:,j+2*d] = [x[i][j]^2 for i=1:n]
+    end
+    β = pinv(X'*X)*X'*y
+
+    return SecondOrderPolynomialSurrogate(X,y,β,lb,ub)
 end
 
-function my_second_ord(val)
-    #TODO
-    return prediction
+function (my_second_ord::SecondOrderPolynomialSurrogate)(val)
+    #just create the val vector as X and multiply
+    d = length(val)
+    X = ones(eltype(val[1]),1,3*d)
+    for j = 1:d
+        X[j+1] = val[j]
+    end
+    for j = 1:d-1
+        X[j+d+1] = val[j]*val[j+1]
+    end
+    for j = 1:d
+        X[j+2*d] = val[j]^2
+    end
+    return X*my_second_ord.β
 end
 
 function add_point!(my_second::SecondOrderPolynomialSurrogate,x_new,y_new)
@@ -44,7 +69,8 @@ function add_point!(my_second::SecondOrderPolynomialSurrogate,x_new,y_new)
         my_second.β = pinv(my_second.x'*my_second.x)*my_second.x'*my_second.y
     else
         #ND
-        #TODO
+        my_second.y = vcat(my_second.y,y_new)
+        my_second.β = pinv(my_second.x'*my_second.x)*my_second.x'*my_second.y
     end
     nothing
 end
--- test/secondOrderPolynomialSurrogate.jl
+++ test/secondOrderPolynomialSurrogate.jl
@@ -1,6 +1,7 @@
 using Surrogates
 
 #1D
+#=
 lb = 0.0
 ub = 5.0
 obj_1D = x -> log(x)*exp(x)
@@ -10,8 +11,8 @@ my_second_order_poly = SecondOrderPolynomialSurrogate(x,y,lb,ub)
 val = my_second_order_poly(5.0)
 add_point!(my_second_order_poly,5.0,238.86)
 add_point!(my_second_order_poly,[6.0,7.0],[722.84,2133.94])
+=#
 
-#=
 #ND
 lb = [0.0,0.0]
 ub = [10.0,10.0]
@@ -20,6 +21,7 @@ x = sample(5,lb,ub,UniformSample())
 y = obj_ND.(x)
 my_second_order_poly = SecondOrderPolynomialSurrogate(x,y,lb,ub)
 val = my_second_order_poly((5.0,7.0))
+#=
 add_point!(my_second_order_poly,(5.0,7.0),1764.96)
 add_point!(my_second_order_poly,[(1.5,1.5),(3.4,5.4)],[1.817,270.95])
 =#
