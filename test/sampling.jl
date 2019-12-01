28008187093630f339c200ba6f7ab89d3ecc761d
ludoro <ludovicobessi@gmail.com>
2019-12-01 21:38:52 +0100

    Fixed sampling shape issue

--- test/sampling.jl
+++ test/sampling.jl
@@ -1,5 +1,7 @@
 using Surrogates
 using Distributions
+using Test
+
 #1D
 lb = 0.0
 ub = 5.0
@@ -18,10 +20,31 @@ lb = [0.1,-0.5]
 ub = [1.0,20.0]
 n = 5
 d = 2
+
+#GridSample{T}
 s = Surrogates.sample(n,lb,ub,GridSample([0.1,0.5]))
-Surrogates.sample(n,lb,ub,UniformSample())
-Surrogates.sample(n,lb,ub,SobolSample())
-Surrogates.sample(n,lb,ub,LatinHypercubeSample())
-Surrogates.sample(n,lb,ub,LowDiscrepancySample([10,3]))
-Surrogates.sample(n,d,Cauchy())
-Surrogates.sample(n,d,Normal(3,5))
+@test isa(s,Array{Tuple{typeof(s[1][1]),typeof(s[1][1])},1}) == true
+
+#UniformSample()
+s = Surrogates.sample(n,lb,ub,UniformSample())
+@test isa(s,Array{Tuple{typeof(s[1][1]),typeof(s[1][1])},1}) == true
+
+#SobolSample()
+s = Surrogates.sample(n,lb,ub,SobolSample())
+@test isa(s,Array{Tuple{typeof(s[1][1]),typeof(s[1][1])},1}) == true
+
+#LHS
+s = Surrogates.sample(n,lb,ub,LatinHypercubeSample())
+@test isa(s,Array{Tuple{typeof(s[1][1]),typeof(s[1][1])},1}) == true
+
+#LDS
+s = Surrogates.sample(n,lb,ub,LowDiscrepancySample([10,3]))
+@test isa(s,Array{Tuple{typeof(s[1][1]),typeof(s[1][1])},1}) == true
+
+#Distribution 1
+s = Surrogates.sample(n,d,Cauchy())
+@test isa(s,Array{Tuple{typeof(s[1][1]),typeof(s[1][1])},1}) == true
+
+#Distribution 2
+s = Surrogates.sample(n,d,Normal(3,5))
+@test isa(s,Array{Tuple{typeof(s[1][1]),typeof(s[1][1])},1}) == true
