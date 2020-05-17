f4828f43ea3de1e34f21be9883567bc677a327f9
ludoro <ludovicobessi@gmail.com>
2020-05-17 22:28:39 +0200

    Added kwargs for Kriging

--- test/Kriging.jl
+++ test/Kriging.jl
@@ -12,6 +12,38 @@ my_k = Kriging(x,y,p)
 add_point!(my_k,4.0,75.68)
 add_point!(my_k,[5.0,6.0],[238.86,722.84])
 pred = my_k(5.5)
+#WITHOUT ADD POINT
+x = [1.0,2.0,3.0]
+y = [4.0,5.0,6.0]
+p = 1.3
+my_k = Kriging(x,y,p)
+est = my_k(1.0)
+std_err = std_error_at_point(my_k,1.0)
+@test std_err < 10^(-6)
+
+#WITH ADD POINT adding singleton
+x = [1.0,2.0,3.0]
+y = [4.0,5.0,6.0]
+p = 1.3
+my_k = Kriging(x,y,p)
+add_point!(my_k,4.0,9.0)
+est = my_k(4.0)
+std_err = std_error_at_point(my_k,4.0)
+@test std_err < 10^(-6)
+
+
+#WITH ADD POINT adding more
+x = [1.0,2.0,3.0]
+y = [4.0,5.0,6.0]
+p = 1.3
+my_k = Kriging(x,y,p)
+add_point!(my_k,[4.0,5.0,6.0],[9.0,13.0,15.0])
+est = my_k(4.0)
+std_err = std_error_at_point(my_k,4.0)
+@test std_err < 10^(-6)
+
+#Testin kwargs 1D
+kwar_krig = Kriging(x,y);
 
 
 #ND
@@ -62,32 +94,5 @@ est = my_k((10.0,11.0,12.0))
 std_err = std_error_at_point(my_k,(10.0,11.0,12.0))
 @test std_err < 10^(-6)
 
-#WITHOUT ADD POINT
-x = [1.0,2.0,3.0]
-y = [4.0,5.0,6.0]
-p = 1.3
-my_k = Kriging(x,y,p)
-est = my_k(1.0)
-std_err = std_error_at_point(my_k,1.0)
-@test std_err < 10^(-6)
-
-#WITH ADD POINT adding singleton
-x = [1.0,2.0,3.0]
-y = [4.0,5.0,6.0]
-p = 1.3
-my_k = Kriging(x,y,p)
-add_point!(my_k,4.0,9.0)
-est = my_k(4.0)
-std_err = std_error_at_point(my_k,4.0)
-@test std_err < 10^(-6)
-
-
-#WITH ADD POINT adding more
-x = [1.0,2.0,3.0]
-y = [4.0,5.0,6.0]
-p = 1.3
-my_k = Kriging(x,y,p)
-add_point!(my_k,[4.0,5.0,6.0],[9.0,13.0,15.0])
-est = my_k(4.0)
-std_err = std_error_at_point(my_k,4.0)
-@test std_err < 10^(-6)
+#test kwargs ND
+kwarg_krig_ND = Kriging(x,y)
