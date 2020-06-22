067ebab69c0ea8e50226d165d292e0eae9c96fab
Ludovico Bessi <ludovicobessi@gmail.com>
2020-06-22 12:20:34 +0200

    MOE ND and tests done

--- src/MOE.jl
+++ src/MOE.jl
@@ -133,20 +133,69 @@ end
 function MOE(x,y,lb,ub; k::Int = 2,
             local_kind = [RadialBasisStructure(radial_function = linearRadial, scale_factor=1.0, sparse = false),RadialBasisStructure(radial_function = cubicRadial, scale_factor=1.0, sparse = false)])
 
-
-    #find varcov matrix for each mixture
-    X_G = collect(reshape(collect(Base.Iterators.flatten(x)), (length(x[1]),length(x)))')
+    n = length(x)
+    d = length(lb)
+    #GMM parameters:
+    X_G = collect(reshape(collect(Base.Iterators.flatten(x)), (d,n))')
+    my_gmm = GMM(k,X_G,kind = :full)
+    weights = my_gmm.w
+    means = my_gmm.μ
+    varcov = my_gmm.Σ
 
     #cluster the points
-    X_C = collect(reshape(collect(Base.Iterators.flatten(x)), (length(x[1]),length(x))))
+    X_C = collect(reshape(collect(Base.Iterators.flatten(x)), (d,n)))
+    KNN = kmeans(X_C, k)
+    x_c = [ Array{eltype(x)}(undef,0) for i = 1:k]
+    y_c = [ Array{eltype(y)}(undef,0) for i = 1:k]
+    a = assignments(KNN)
+    @inbounds for i = 1:n
+        pos = a[i]
+        x_c[pos] = vcat(x_c[pos],x[i])
+        append!(y_c[pos],y[i])
+    end
 
+    local_surr = Dict()
+    for i = 1:k
+        if local_kind[i][1] == "RadialBasis"
+            #fit and append to local_surr
+            my_local_i = RadialBasis(x_c[i],y_c[i],lb,ub,rad = local_kind[i].radial_function, scale_factor = local_kind[i].scale_factor, sparse = local_kind[i].sparse)
+            local_surr[i] = my_local_i
 
-    #fit each cluster with a Surrogate
+        elseif local_kind[i][1] == "Kriging"
+            my_local_i = Kriging(x_c[i], y_c[i],lb,ub, p = local_kind[i].p, theta = local_kind[i].p)
+            local_surr[i] = my_local_i
 
+        elseif local_kind[i] == "LinearSurrogate"
+            my_local_i = LinearSurrogate(x_c[i], y_c[i],lb,ub)
+            local_surr[i] = my_local_i
 
-    #find varcov matrix for each mixture
+        elseif local_kind[i][1] == "InverseDistanceSurrogate"
+            my_local_i = InverseDistanceSurrogate(x_c[i], y_c[i],lb,ub, local_kind[i].p)
+            local_surr[i] = my_local_i
 
+        elseif local_kind[i][1] == "LobacheskySurrogate"
+            my_local_i = LobacheskySurrogate(x_c[i], y_c[i],lb,ub,alpha = local_kind[i].alpha , n = local_kind[i].n, sparse = local_kind[i].sparse)
+            local_surr[i] = my_local_i
 
+        elseif local_kind[i][1] == "NeuralSurrogate"
+            my_local_i = NeuralSurrogate(x_c[i], y_c[i],lb,ub, model = local_kind[i].model , loss = local_kind[i].loss ,opt = local_kind[i].opt ,n_echos = local_kind[i].n_echos)
+            local_surr[i] = my_local_i
+
+        elseif local_kind[i][1] == "RandomForestSurrogate"
+            my_local_i = RandomForestSurrogate(x_c[i], y_c[i],lb,ub, num_round = local_kind[i].num_round)
+            local_surr[i] = my_local_i
+
+        elseif local_kind[i] == "SecondOrderPolynomialSurrogate"
+            my_local_i = SecondOrderPolynomialSurrogate(x_c[i], y_c[i],lb,ub)
+            local_surr[i] = my_local_i
+
+        elseif local_kind[i][1] == "Wendland"
+            my_local_i = Wendand(x_c[i], y_c[i],lb,ub, eps = local_kind[i].eps, maxiters = local_kind[i].maxiters, tol = local_kind[i].tol)
+            local_surr[i] = my_local_i
+        else
+            throw("A surrogate with name "* local_kind[i][1] *" does not exist or is not currently supported with MOE.")
+        end
+    end
     return MOE(x,y,lb,ub,local_surr,k,means,varcov,weights)
 end
 
@@ -193,7 +242,27 @@ function add_point!(moe::MOE,x_new,y_new)
             add_point!(moe.local_surr[i],moe.x[n-n_added+i],moe.y[n-n_added+i])
         end
     else
-        #todo
+        #ND
+        moe.x = vcat(moe.x,x_new)
+        moe.y = vcat(moe.y,y_new)
+        n = length(moe.x)
+        d = length(moe.lb)
+        #New mixture parameters
+        X_G = collect(reshape(collect(Base.Iterators.flatten(moe.x)), (d,n))')
+        my_gmm = GMM(moe.k,X_G,kind = :full)
+        moe.weights = my_gmm.w
+        moe.means = my_gmm.μ
+        moe.varcov = my_gmm.Σ
+
+        #cluster the points
+        X_C = collect(reshape(collect(Base.Iterators.flatten(moe.x)), (d,n)))
+        KNN = kmeans(X_C, moe.k)
+        a = assignments(KNN)
+        n_added = length(x_new)
+        for i = 1:n_added
+            pos = a[n-n_added+i]
+            add_point!(moe.local_surr[i],moe.x[n-n_added+i],moe.y[n-n_added+i])
+        end
     end
     nothing
 end
--- test/MOE.jl
+++ test/MOE.jl
@@ -1,10 +1,13 @@
 using Surrogates
-
+using Distributions
 #1D MOE
+
 n = 4
 lb = 0.0
 ub = 5.0
-x = sample(n,lb,ub,SobolSample())
+x1 = Surrogates.sample(n,1,Normal(2.5,0.1))
+x2 = Surrogates.sample(n,1,Normal(1.0,0.4))
+x = vcat(x1,x2)
 f = x-> 2*x
 y = f.(x)
 #Standard definition
@@ -20,4 +23,22 @@ my_moe = MOE(x,y,lb,ub,k = 2,local_kind = my_local_kind)
 
 
 
-#ND MOE 
+#ND MOE
+
+n = 10
+lb = [0.0,0.0]
+ub = [5.0,5.0]
+x1 = Surrogates.sample(n,2,Normal(0.0,4.0))
+x2 = Surrogates.sample(n,2,Normal(3.0,5.0))
+x = vcat(x1,x2)
+f = x -> x[1]*x[2]
+y = f.(x)
+my_moe_ND = MOE(x,y,lb,ub)
+val = my_moe_ND((1.0,1.0))
+
+add_point!(my_moe_ND, (1.0,1.0), 1.0)
+
+#Local surr redefinition
+my_locals = [InverseDistanceStructure(p = 2.0),
+             SecondOrderPolynomialStructure()]
+my_moe_redef = MOE(x,y,lb,ub,k = 2,local_kind = my_local_kind)
