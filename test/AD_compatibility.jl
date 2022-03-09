55678d2f32dc2fb01bea8969dd7b3d60bc7fcdeb
Vikram <vikramsnarayan@gmail.com>
2022-03-09 12:49:28 +0530

    wrap with @testset

--- test/AD_compatibility.jl
+++ test/AD_compatibility.jl
@@ -110,40 +110,53 @@ f = x -> x^2
 y = f.(x)
 
 #Radials
+@testset "Radials 1D" begin
 my_rad = RadialBasis(x,y,lb,ub,rad = linearRadial)
 g = x -> my_rad'(x)
 g(5.0)
+end
 
 #Kriging
+@testset "Kriging 1D" begin
 my_p = 1.5
 my_krig = Kriging(x,y,lb,ub,p=my_p)
 g = x -> my_krig'(x)
 g(5.0)
+end
 
 #Linear Surrogate
+@testset "Linear Surrogate" begin
 my_linear = LinearSurrogate(x,y,lb,ub)
 g = x -> my_linear'(x)
 g(5.0)
+end
 
 #Inverse distance
+@testset "Inverse Distance" begin
 my_p = 1.4
 my_inverse = InverseDistanceSurrogate(x,y,lb,ub,p=my_p)
 g = x -> my_inverse'(x)
 g(5.0)
+end
 
 #Second order polynomial
+@testset "Second Order Polynomial" begin
 my_second = SecondOrderPolynomialSurrogate(x,y,lb,ub)
 g = x -> my_second'(x)
 g(5.0)
+end 
 
 #Lobachevsky
+@testset "Lobachevsky" begin
 n = 4
 α = 2.4
 my_loba = LobachevskySurrogate(x,y,lb,ub, alpha = α, n = 4)
 g = x -> my_loba'(x)
 g(0.0)
+end
 
 #NN
+@testset "NN" begin
 my_model = Chain(Dense(1,1), first)
 my_loss(x, y) = Flux.mse(my_model(x), y)
 my_opt = Descent(0.01)
@@ -151,25 +164,30 @@ n_echos = 1
 my_neural = NeuralSurrogate(x,y,lb,ub,model=my_model,loss=my_loss,opt=my_opt,n_echos=1)
 g = x->my_neural'(x)
 g(3.4)
+end
 
 #Wendland
+@testset "Wendland" begin
 my_wend = Wendland(x,y,lb,ub)
 g = x -> my_wend'(x)
 g(3.0)
+end
 
 #MOE and VariableFidelity for free because they are Linear combinations
 #of differentiable surrogates
 
 #Polynomialchaos
+@testset "Polynomial Chaos" begin
 n = 50
 x = sample(n,lb,ub,SobolSample())
 y = f.(x)
 my_poli = PolynomialChaosSurrogate(x,y,lb,ub)
 g = x -> my_poli'(x)
 g(3.0)
-
+end
 
 #Gek
+@testset "GEK" begin
 n = 10
 lb = 0.0
 ub = 5.0
@@ -183,6 +201,7 @@ y = vcat(y1,y2)
 my_gek = GEK(x,y,lb,ub)
 g = x-> my_gek'(x)
 g(3.0)
+end
 
 ################
 ###### ND ######
@@ -196,28 +215,35 @@ f = x -> x[1]*x[2]
 y = f.(x)
 
 #Radials
+@testset "Radials ND" begin
 my_rad = RadialBasis(x,y,lb,ub,rad = linearRadial, scale_factor = 2.1)
 g = x -> Zygote.gradient(my_rad,x)
 g((2.0,5.0))
+end
 
 #Kriging
+@testset "Kriging ND" begin
 my_theta = [2.0,2.0]
 my_p = [1.9,1.9]
 my_krig = Kriging(x,y,lb,ub,p=my_p,theta=my_theta)
 g = x -> Zygote.gradient(my_krig,x)
 g((2.0,5.0))
+end
 
 #Linear Surrogate
+@testset "Linear Surrogate ND" begin
 my_linear = LinearSurrogate(x,y,lb,ub)
 g = x -> Zygote.gradient(my_linear,x)
 g((2.0,5.0))
+end
 
 #Inverse Distance
+@testset "Inverse Distance ND" begin
 my_p = 1.4
 my_inverse = InverseDistanceSurrogate(x,y,lb,ub,p=my_p)
 g = x -> Zygote.gradient(my_inverse,x)
 g((2.0,5.0))
-
+end
 
 
 #Lobachevsky not working yet weird issue with Zygote @nograd
@@ -231,11 +257,14 @@ g((2.0,5.0))
 =#
 
 #Second order polynomial mutating arrays
+@testset "SecondOrderPolynomialSurrogate ND" begin
 my_second = SecondOrderPolynomialSurrogate(x,y,lb,ub)
 g = x -> Zygote.gradient(my_second,x)
 g((2.0,5.0))
+end 
 
 #NN
+@testset "NN ND" begin
 my_model = Chain(Dense(2,1), first)
 my_loss(x, y) = Flux.mse(my_model(x), y)
 my_opt = Descent(0.01)
@@ -243,17 +272,20 @@ n_echos = 1
 my_neural = NeuralSurrogate(x,y,lb,ub,model=my_model,loss=my_loss,opt=my_opt,n_echos=1)
 g = x -> Zygote.gradient(my_neural, x)
 g((2.0,5.0))
+end
 
 #wendland
+@testset "Wendland ND" begin
 my_wend_ND = Wendland(x,y,lb,ub)
 g = x -> Zygote.gradient(my_wend_ND,x)
 g((2.0,5.0))
-
+end
 #MOE and VariableFidelity for free because they are Linear combinations
 #of differentiable surrogates
 
 
 #PolynomialChaos
+@testset "Polynomial Chaos ND" begin
 n = 50
 lb = [0.0,0.0]
 ub = [10.0,10.0]
@@ -288,7 +320,7 @@ y = vcat(y1,y2)
 my_gek_ND = GEK(x,y,lb,ub)
 g = x -> Zygote.gradient(my_gek_ND,x)
 g((2.0,5.0))
-
+end
 ###### ND -> ND ######
 
 lb = [0.0, 0.0]
@@ -299,6 +331,7 @@ f = x -> [x[1]^2, x[2]]
 y = f.(x)
 
 #NN
+@testset "NN ND -> ND" begin
 my_model = Chain(Dense(2,2))
 my_loss(x, y) = Flux.mse(my_model(x), y)
 my_opt = Descent(0.01)
@@ -316,3 +349,4 @@ Zygote.gradient(x -> sum(my_inverse(x)), (2.0, 5.0))
 
 my_second = SecondOrderPolynomialSurrogate(x,y,lb,ub)
 Zygote.gradient(x -> sum(my_second(x)), (2.0, 5.0))
+end
