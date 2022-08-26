2d9f858c04c0023154b5da3462a68f9ba2e23229
Vikram <vikramsnarayan@gmail.com>
2022-08-26 11:28:36 -0400

    clean up and add more tests

--- lib/SurrogatesMOE/src/SurrogatesMOE.jl
+++ lib/SurrogatesMOE/src/SurrogatesMOE.jl
@@ -3,10 +3,10 @@ module SurrogatesMOE
 import Surrogates: AbstractSurrogate, linearRadial, cubicRadial, multiquadricRadial,
                    thinplateRadial, RadialBasisStructure, RadialBasis,
                    InverseDistanceSurrogate, Kriging, LobachevskyStructure,
-                   LobachevskySurrogate, NeuralStructure, PolyChaosStructure, LinearSurrogate
+                   LobachevskySurrogate, NeuralStructure, PolyChaosStructure,
+                   LinearSurrogate
 
 export MOE
-
 
 using GaussianMixtures
 using Random
@@ -17,7 +17,6 @@ using SurrogatesFlux
 using SurrogatesPolyChaos
 using SurrogatesRandomForest
 using XGBoost
-
 
 mutable struct MOE{X, Y, C, D, M} <: AbstractSurrogate
     x::X
@@ -31,31 +30,32 @@ end
     MOE(x, y, expert_types;  ndim=1, n_clusters=2)
 constructor for MOE; takes in x, y and expert types and returns an MOE struct
 """
-function MOE(x, y, expert_types;  ndim=1, n_clusters=2)
+function MOE(x, y, expert_types; ndim = 1, n_clusters = 2)
     quantile = 10
-    if(ndim>1)
+    if (ndim > 1)
         x = _vector_of_tuples_to_matrix(x)
     end
-    values = hcat(x,y)
-    
+    values = hcat(x, y)
 
-    x_and_y_test, x_and_y_train = _extract_part(values, quantile)    
+    x_and_y_test, x_and_y_train = _extract_part(values, quantile)
     # We get posdef error without jitter; And if values repeat we get NaN vals 
     # https://github.com/davidavdav/GaussianMixtures.jl/issues/21 
-    jitter_vals = ((rand(eltype(x_and_y_train), size(x_and_y_train)))./10000)
-    gm_cluster = GMM(n_clusters, x_and_y_train+jitter_vals, kind=:full, nInit=50, nIter=20)
-    mvn_distributions = _create_clusters_distributions(gm_cluster, ndim, n_clusters) 
+    jitter_vals = ((rand(eltype(x_and_y_train), size(x_and_y_train))) ./ 10000)
+    gm_cluster = GMM(n_clusters, x_and_y_train + jitter_vals, kind = :full, nInit = 50,
+                     nIter = 20)
+    mvn_distributions = _create_clusters_distributions(gm_cluster, ndim, n_clusters)
     cluster_classifier_train = _cluster_predict(gm_cluster, x_and_y_train)
     clusters_train = _cluster_values(x_and_y_train, cluster_classifier_train, n_clusters)
     cluster_classifier_test = _cluster_predict(gm_cluster, x_and_y_test)
     clusters_test = _cluster_values(x_and_y_test, cluster_classifier_test, n_clusters)
     best_models = []
     for i in 1:n_clusters
-        best_model = _find_best_model(clusters_train[i], clusters_test[i], ndim, expert_types)
+        best_model = _find_best_model(clusters_train[i], clusters_test[i], ndim,
+                                      expert_types)
         push!(best_models, best_model)
     end
     X = values[:, 1:ndim]
-    y = values[:, 2] 
+    y = values[:, 2]
     return MOE(X, y, gm_cluster, mvn_distributions, best_models)
 end
 
@@ -73,11 +73,9 @@ function (moe::MOE)(val::Number)
         probs = probs / rad
     end
     max_index = argmax(probs)
-    prediction = moe.m[max_index](val[1]) 
+    prediction = moe.m[max_index](val[1])
     return prediction
 end
-
-
 
 """
     (moe::MOE)(val)
@@ -97,7 +95,7 @@ function (moe::MOE)(val)
     end
 
     max_index = argmax(probs)
-    prediction = moe.m[max_index](val) 
+    prediction = moe.m[max_index](val)
     return prediction
 end
 
@@ -132,14 +130,13 @@ end
     train #  [3.0  4.0; 5.0  6.0; 7.0  8.0]
 """
 function _extract_part(values, quantile)
-    num = size(values,1)
+    num = size(values, 1)
     indices = collect(1:quantile:num)
     mask = BitArray(undef, num)
-    mask[indices].= true
+    mask[indices] .= true
     #mask
-    return values[mask, :], values[.~mask,:]
+    return values[mask, :], values[.~mask, :]
 end
-
 
 """
     _cluster_values(values, cluster_classifier, num_clusters)
@@ -163,12 +160,12 @@ clusters = _cluster_values(vals, cluster_classifier, num_clusters)
 """
 function _cluster_values(values, cluster_classifier, num_clusters)
     num = length(cluster_classifier)
-    if (size(values,1) != num)
+    if (size(values, 1) != num)
         error("Number of values don't match number of cluster_classifier points")
     end
     clusters = [[] for n in 1:num_clusters]
     for i in 1:num
-        push!(clusters[cluster_classifier[i]],(values[i, :]))
+        push!(clusters[cluster_classifier[i]], (values[i, :]))
     end
     return clusters
 end
@@ -182,7 +179,7 @@ n_clusters - number of clusters
 output
 distribs - a vector containing frozen multivariate normal distributions for each cluster
 """
-function _create_clusters_distributions(gmm::GMM, ndim, n_clusters)    
+function _create_clusters_distributions(gmm::GMM, ndim, n_clusters)
     means = gmm.μ
     cov = covars(gmm)
     distribs = []
@@ -207,7 +204,7 @@ function _find_upper_lower_bounds(X::Matrix)
         push!(ub, findmax(col)[1])
         push!(lb, findmin(col)[1])
     end
-    if(size(X, 2)==1)
+    if (size(X, 2) == 1)
         return lb[1][1], ub[1][1]
     else
         return lb, ub
@@ -219,37 +216,39 @@ _find_best_model(clustered_values, clustered_test_values)
 finds best model for each set of clustered values by validating against the clustered_test_values
 
 """
-function _find_best_model(clustered_train_values, clustered_test_values, dim, enabled_expert_types)
+function _find_best_model(clustered_train_values, clustered_test_values, dim,
+                          enabled_expert_types)
     # find upper and lower bounds for clustered_train and test values concatenated
 
-    x_vec = [a[1:dim] for a in clustered_train_values] 
-    y_vec = [last(a) for a in clustered_train_values] 
+    x_vec = [a[1:dim] for a in clustered_train_values]
+    y_vec = [last(a) for a in clustered_train_values]
 
-    x_test_vec = [a[1:dim] for a in clustered_test_values] 
+    x_test_vec = [a[1:dim] for a in clustered_test_values]
     y_test_vec = [last(a) for a in clustered_test_values]
 
     if (dim == 1)
         xtrain_mat = reshape(x_vec, (size(clustered_train_values, 1), dim))
         xtest_mat = reshape(x_test_vec, (size(clustered_test_values, 1), dim))
-    else 
+    else
         xtrain_mat = _vector_of_tuples_to_matrix(x_vec)
         xtest_mat = _vector_of_tuples_to_matrix(x_test_vec)
     end
 
     X = vcat(xtrain_mat, xtest_mat)
     lb, ub = _find_upper_lower_bounds(X)
-    
+
     # call on _surrogate_builder with clustered_train_vals, enabled expert types, lb, ub 
 
-    surr_vec = _surrogate_builder(enabled_expert_types, length(enabled_expert_types), x_vec, y_vec, lb, ub) 
+    surr_vec = _surrogate_builder(enabled_expert_types, length(enabled_expert_types), x_vec,
+                                  y_vec, lb, ub)
 
     # use the models to find best model after validating against test data and return best model 
     best_rmse = Inf
     best_model = surr_vec[1] #initial assignment can be any model
-    for surr_model in surr_vec 
+    for surr_model in surr_vec
         pred = surr_model.(x_test_vec)
-        rmse = norm(pred-y_test_vec, 2)
-        if(rmse < best_rmse)
+        rmse = norm(pred - y_test_vec, 2)
+        if (rmse < best_rmse)
             best_rmse = rmse
             best_model = surr_model
         end
@@ -271,68 +270,68 @@ function _surrogate_builder(local_kind, k, x, y, lb, ub)
                                      rad = local_kind[i].radial_function,
                                      scale_factor = local_kind[i].scale_factor,
                                      sparse = local_kind[i].sparse)
-            push!(local_surr,  my_local_i)
+            push!(local_surr, my_local_i)
 
         elseif local_kind[i][1] == "Kriging"
             #because Kriging takes abs of two vectors
-            if(length(lb) == 1)
-                x = [a[1] for a in x] 
+            if (length(lb) == 1)
+                x = [a[1] for a in x]
             end
 
             my_local_i = Kriging(x, y, lb, ub, p = local_kind[i].p,
                                  theta = local_kind[i].theta)
-            push!(local_surr,  my_local_i)
+            push!(local_surr, my_local_i)
 
         elseif local_kind[i][1] == "GEK"
             my_local_i = GEK(x, y, lb, ub, p = local_kind[i].p,
                              theta = local_kind[i].theta)
-            push!(local_surr,  my_local_i)
+            push!(local_surr, my_local_i)
 
         elseif local_kind[i] == "LinearSurrogate"
             my_local_i = LinearSurrogate(x, y, lb, ub)
-            push!(local_surr,  my_local_i)
+            push!(local_surr, my_local_i)
 
         elseif local_kind[i][1] == "InverseDistanceSurrogate"
             my_local_i = InverseDistanceSurrogate(x, y, lb, ub, local_kind[i].p)
-            push!(local_surr,  my_local_i)
+            push!(local_surr, my_local_i)
 
         elseif local_kind[i][1] == "LobachevskySurrogate"
             my_local_i = LobachevskyStructure(x, y, lb, ub,
                                               alpha = local_kind[i].alpha,
                                               n = local_kind[i].n,
                                               sparse = local_kind[i].sparse)
-            push!(local_surr,  my_local_i)
+            push!(local_surr, my_local_i)
 
         elseif local_kind[i][1] == "NeuralSurrogate"
             my_local_i = NeuralSurrogate(x, y, lb, ub,
                                          model = local_kind[i].model,
                                          loss = local_kind[i].loss, opt = local_kind[i].opt,
                                          n_echos = local_kind[i].n_echos)
-            push!(local_surr,  my_local_i)
+            push!(local_surr, my_local_i)
 
         elseif local_kind[i][1] == "RandomForestSurrogate"
             my_local_i = RandomForestSurrogate(x, y, lb, ub,
                                                num_round = local_kind[i].num_round)
-            push!(local_surr,  my_local_i)
+            push!(local_surr, my_local_i)
 
         elseif local_kind[i] == "SecondOrderPolynomialSurrogate"
             my_local_i = SecondOrderPolynomialSurrogate(x, y, lb, ub)
-            push!(local_surr,  my_local_i)
+            push!(local_surr, my_local_i)
 
         elseif local_kind[i][1] == "Wendland"
             my_local_i = Wendand(x, y, lb, ub, eps = local_kind[i].eps,
                                  maxiters = local_kind[i].maxiters, tol = local_kind[i].tol)
-            push!(local_surr,  my_local_i)
+            push!(local_surr, my_local_i)
 
         elseif local_kind[i][1] == "PolynomialChaosSurrogate"
             my_local_i = PolynomialChaosSurrogate(x, y, lb, ub, op = local_kind[i].op)
-            push!(local_surr,  my_local_i)
+            push!(local_surr, my_local_i)
         else
             throw("A surrogate with name provided does not exist or is not currently supported with MOE.")
-        end 
-    end 
+        end
+    end
     return local_surr
-end 
+end
 
 """
     _vector_of_tuples_to_matrix(v)
--- lib/SurrogatesMOE/test/runtests.jl
+++ lib/SurrogatesMOE/test/runtests.jl
@@ -1,91 +1,157 @@
-using Surrogates
-using SurrogatesMOE
-using SurrogatesFlux
-using Flux
-using SurrogatesPolyChaos
-using Test
+# using Surrogates
+# using SurrogatesMOE
+# using SurrogatesFlux
+# using Flux
+# using SurrogatesPolyChaos
+using SafeTestsets
 
-# function discont_1D(x)
-#     if x < 0.0
-#         return -5.0
-#     elseif x >= 0.0
-#         return 5.0
-#     end
-# end
+#test 1D function that is discontinuous
+@safetestset "1D" begin
+    using Surrogates
+    using SurrogatesMOE
+    using SurrogatesFlux
+    using Flux
+    using SurrogatesPolyChaos
 
-# lb = -1.0
-# ub = 1.0
-# x = sample(50, lb, ub, SobolSample())
-# y = discont_1D.(x)
-
-# RAD_1D = RadialBasis(x, y, lb, ub, rad = linearRadial(), scale_factor = 1.0, sparse = false)
-# expert_types = [
-#     RadialBasisStructure(radial_function = linearRadial(), scale_factor = 1.0,
-#                         sparse = false),
-#     RadialBasisStructure(radial_function = cubicRadial(), scale_factor = 1.0,
-#                         sparse = false),
-# ]
-
-# MOE_1D_RAD_RAD = MOE(x,y, expert_types)
-# MOE_at0 = MOE_1D_RAD_RAD(0.0)
-# RAD_at0 = RAD_1D(0.0)
-# true_val = 5.0
-# @test (abs(RAD_at0 - true_val) > abs(MOE_at0 - true_val))
-
-# KRIG_1D = Kriging(x, y, lb, ub, p = 1.0, theta = 1.0)
-# expert_types = [InverseDistanceStructure(p = 1.0),
-#     KrigingStructure(p = 1.0, theta = 1.0)
-#     ]
-# MOE_1D_INV_KRIG = MOE(x,y, expert_types)
-# MOE_at0 = MOE_1D_INV_KRIG(0.0)
-# KRIG_at0 = KRIG_1D(0.0)
-# true_val = 5.0
-# @test (abs(KRIG_at0 - true_val) > abs(MOE_at0 - true_val))
-
-function rmse(a,b)
-    a = vec(a)
-    b = vec(b)
-    if(size(a)!=size(b))
-        println("error in inputs")
-        return
+    function discont_1D(x)
+        if x < 0.0
+            return -5.0
+        elseif x >= 0.0
+            return 5.0
+        end
     end
-    n = size(a,1)
-    return sqrt(sum((a - b).^2)/n)
-end
 
-function discont_NDIM(x)
-    if(x[1] >= 0.0 && x[2] >= 0.0)
-        return sum(x.^2) + 5
-    else
-        return sum(x.^2) - 5
-    end
-end
-lb = [-1.0, -1.0]
-ub = [1.0, 1.0]
-x = sample(200, lb, ub, UniformSample())
-y = discont_NDIM.(x)
-x_test = sample(10, lb, ub, GoldenSample())
+    lb = -1.0
+    ub = 1.0
+    x = sample(50, lb, ub, SobolSample())
+    y = discont_1D.(x)
 
-
-expert_types = [InverseDistanceStructure(p = 1.0),
-    RadialBasisStructure(radial_function = linearRadial(), scale_factor = 1.0,
-                        sparse = false)
+    # Radials vs MOE 
+    RAD_1D = RadialBasis(x, y, lb, ub, rad = linearRadial(), scale_factor = 1.0,
+                         sparse = false)
+    expert_types = [
+        RadialBasisStructure(radial_function = linearRadial(), scale_factor = 1.0,
+                             sparse = false),
+        RadialBasisStructure(radial_function = cubicRadial(), scale_factor = 1.0,
+                             sparse = false),
     ]
-moe_nd_inv_rad = MOE(x,y, expert_types, ndim=2)
-moe_pred_vals = moe_nd_inv_rad.(x_test)
-true_vals = discont_NDIM.(x_test)
-moe_rmse = rmse(true_vals, moe_pred_vals)
-rbf = RadialBasis(x, y, lb, ub)
-rbf_pred_vals = rbf.(x_test)
-rbf_rmse = rmse(true_vals, rbf_pred_vals)
-@test (rbf_rmse > moe_rmse)
 
+    MOE_1D_RAD_RAD = MOE(x, y, expert_types)
+    MOE_at0 = MOE_1D_RAD_RAD(0.0)
+    RAD_at0 = RAD_1D(0.0)
+    true_val = 5.0
+    @test (abs(RAD_at0 - true_val) > abs(MOE_at0 - true_val))
 
-expert_types = [KrigingStructure(p = [1.0, 1.0], theta = [1.0, 1.0]), LinearStructure()]
-MOE_ND_KRIG_LIN = MOE(x, y, expert_types, ndim=2)
-MOE_pred_vals = MOE_ND_KRIG_LIN.(x_test)
-KRIG = Kriging(x, y, lb, ub, p = [1.0, 1.0], theta = [1.0, 1.0])
-krig_pred_vals = KRIG.(x_test)
-krig_rmse = rmse(true_vals, krig_pred_vals)
-moe_rmse = rmse(true_vals, MOE_pred_vals)
-@test krig_rmse > moe_rmse
+    # Krig vs MOE
+    KRIG_1D = Kriging(x, y, lb, ub, p = 1.0, theta = 1.0)
+    expert_types = [InverseDistanceStructure(p = 1.0),
+        KrigingStructure(p = 1.0, theta = 1.0),
+    ]
+    MOE_1D_INV_KRIG = MOE(x, y, expert_types)
+    MOE_at0 = MOE_1D_INV_KRIG(0.0)
+    KRIG_at0 = KRIG_1D(0.0)
+    true_val = 5.0
+    @test (abs(KRIG_at0 - true_val) > abs(MOE_at0 - true_val))
+end
+
+@safetestset "ND" begin
+    using Surrogates
+    using SurrogatesMOE
+    using SurrogatesFlux
+    using Flux
+    using SurrogatesPolyChaos
+
+    # helper to test accuracy of predictors
+    function rmse(a, b)
+        a = vec(a)
+        b = vec(b)
+        if (size(a) != size(b))
+            println("error in inputs")
+            return
+        end
+        n = size(a, 1)
+        return sqrt(sum((a - b) .^ 2) / n)
+    end
+
+    # multidimensional input function
+    function discont_NDIM(x)
+        if (x[1] >= 0.0 && x[2] >= 0.0)
+            return sum(x .^ 2) + 5
+        else
+            return sum(x .^ 2) - 5
+        end
+    end
+    lb = [-1.0, -1.0]
+    ub = [1.0, 1.0]
+    n = 100
+    x = sample(n, lb, ub, UniformSample())
+    y = discont_NDIM.(x)
+    x_test = sample(10, lb, ub, GoldenSample())
+
+    expert_types = [InverseDistanceStructure(p = 1.0),
+        RadialBasisStructure(radial_function = linearRadial(), scale_factor = 1.0,
+                             sparse = false),
+    ]
+    moe_nd_inv_rad = MOE(x, y, expert_types, ndim = 2)
+    moe_pred_vals = moe_nd_inv_rad.(x_test)
+    true_vals = discont_NDIM.(x_test)
+    moe_rmse = rmse(true_vals, moe_pred_vals)
+    rbf = RadialBasis(x, y, lb, ub)
+    rbf_pred_vals = rbf.(x_test)
+    rbf_rmse = rmse(true_vals, rbf_pred_vals)
+    @test (rbf_rmse > moe_rmse)
+
+    expert_types = [KrigingStructure(p = [1.0, 1.0], theta = [1.0, 1.0]), LinearStructure()]
+    MOE_ND_KRIG_LIN = MOE(x, y, expert_types, ndim = 2)
+    MOE_pred_vals = MOE_ND_KRIG_LIN.(x_test)
+    krig = Kriging(x, y, lb, ub, p = [1.0, 1.0], theta = [1.0, 1.0])
+    krig_pred_vals = krig.(x_test)
+    krig_rmse = rmse(true_vals, krig_pred_vals)
+    moe_rmse = rmse(true_vals, MOE_pred_vals)
+    @test krig_rmse > moe_rmse
+end
+
+@safetestset "Miscellaneous" begin
+    using Surrogates
+    using SurrogatesMOE
+    using SurrogatesFlux
+    using Flux
+    using SurrogatesPolyChaos
+
+    # multidimensional input function
+    function discont_NDIM(x)
+        if (x[1] >= 0.0 && x[2] >= 0.0)
+            return sum(x .^ 2) + 5
+        else
+            return sum(x .^ 2) - 5
+        end
+    end
+    lb = [-1.0, -1.0]
+    ub = [1.0, 1.0]
+    n = 100
+    x = sample(n, lb, ub, UniformSample())
+    y = discont_NDIM.(x)
+    x_test = sample(10, lb, ub, GoldenSample())
+
+    # test if MOE handles 3 experts including SurrogatesFlux
+    expert_types = [
+        RadialBasisStructure(radial_function = linearRadial(), scale_factor = 1.0,
+                             sparse = false),
+        LinearStructure(),
+        InverseDistanceStructure(p = 1.0),
+    ]
+    moe_nd_3_experts = MOE(x, y, expert_types, ndim = 2, n_clusters = 3)
+    moe_pred_vals = moe_nd_3_experts.(x_test)
+
+    # test if MOE handles SurrogatesFlux
+    model = Chain(Dense(2, 1), first)
+    loss(x, y) = Flux.mse(model(x), y)
+    opt = Descent(0.01)
+    n_echos = 1
+    expert_types = [
+        NeuralStructure(model = model, loss = loss, opt = opt, n_echos = n_echos),
+        LinearStructure(),
+    ]
+    moe_nn_ln = MOE(x, y, expert_types, ndim = 2)
+    moe_pred_vals = moe_nn_ln.(x_test)
+end
