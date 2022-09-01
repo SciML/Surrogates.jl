03fdc6b6acc77bf5087923b2a7b34146385cf45a
Vikram <vikramsnarayan@gmail.com>
2022-09-01 12:06:46 -0400

    test only PolyChaos

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,26 +7,27 @@ function dev_subpkg(subpkg)
     subpkg_path = joinpath(dirname(@__DIR__), "lib", subpkg)
     Pkg.develop(PackageSpec(path = subpkg_path))
 end
-for pkg in ["SurrogatesAbstractGPs", "SurrogatesFlux", "SurrogatesPolyChaos",
-    "SurrogatesRandomForest", "SurrogatesSVM", "SurrogatesMOE"]
+for pkg in ["SurrogatesPolyChaos"]
+# for pkg in ["SurrogatesAbstractGPs", "SurrogatesFlux", "SurrogatesPolyChaos",
+#     "SurrogatesRandomForest", "SurrogatesSVM", "SurrogatesMOE"]
     @time begin
         dev_subpkg(pkg)
         Pkg.test(pkg)
     end
 end
 
-@time @safetestset "GEKPLS.jl" begin include("GEKPLS.jl") end
-@time @safetestset "Radials.jl" begin include("Radials.jl") end
-@time @safetestset "Kriging.jl" begin include("Kriging.jl") end
-@time @safetestset "Sampling" begin include("sampling.jl") end
-@time @safetestset "Optimization" begin include("optimization.jl") end
-@time @safetestset "LinearSurrogate" begin include("linearSurrogate.jl") end
-@time @safetestset "Lobachevsky" begin include("lobachevsky.jl") end
-@time @safetestset "InverseDistanceSurrogate" begin include("inverseDistanceSurrogate.jl") end
-@time @safetestset "SecondOrderPolynomialSurrogate" begin include("secondOrderPolynomialSurrogate.jl") end
-# @time @safetestset  "AD_Compatibility" begin include("AD_compatibility.jl") end
-@time @safetestset "Wendland" begin include("Wendland.jl") end
-@time @safetestset "VariableFidelity" begin include("VariableFidelity.jl") end
-@time @safetestset "Earth" begin include("earth.jl") end
-@time @safetestset "Gradient Enhanced Kriging" begin include("GEK.jl") end
-@time @safetestset "Section Samplers" begin include("SectionSampleTests.jl") end
+# @time @safetestset "GEKPLS.jl" begin include("GEKPLS.jl") end
+# @time @safetestset "Radials.jl" begin include("Radials.jl") end
+# @time @safetestset "Kriging.jl" begin include("Kriging.jl") end
+# @time @safetestset "Sampling" begin include("sampling.jl") end
+# @time @safetestset "Optimization" begin include("optimization.jl") end
+# @time @safetestset "LinearSurrogate" begin include("linearSurrogate.jl") end
+# @time @safetestset "Lobachevsky" begin include("lobachevsky.jl") end
+# @time @safetestset "InverseDistanceSurrogate" begin include("inverseDistanceSurrogate.jl") end
+# @time @safetestset "SecondOrderPolynomialSurrogate" begin include("secondOrderPolynomialSurrogate.jl") end
+# # @time @safetestset  "AD_Compatibility" begin include("AD_compatibility.jl") end
+# @time @safetestset "Wendland" begin include("Wendland.jl") end
+# @time @safetestset "VariableFidelity" begin include("VariableFidelity.jl") end
+# @time @safetestset "Earth" begin include("earth.jl") end
+# @time @safetestset "Gradient Enhanced Kriging" begin include("GEK.jl") end
+# @time @safetestset "Section Samplers" begin include("SectionSampleTests.jl") end
