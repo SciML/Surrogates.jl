2eb4353eaa4dd317944e1687861d0efd2fecd955
Dreycen Foiles <foilesdreycen@gmail.com>
2023-05-20 18:13:11 -0500

    Added 6 parallelization strategies for Kriging

--- src/Kriging.jl
+++ src/Kriging.jl
@@ -261,3 +261,39 @@ function add_point!(k::Kriging, new_x, new_y)
     k.mu, k.b, k.sigma, k.inverse_of_R = _calc_kriging_coeffs(k.x, k.y, k.p, k.theta)
     nothing
 end
+
+# Minimum Constant Liar 
+function CLmin!(tmp_k::Kriging, k::Kriging, new_x)
+    new_y = minimum(k.y)
+    add_point!(tmp_k, new_x, new_y)
+end
+
+# Maximum Constant Liar
+function CLmax!(tmp_k::Kriging, k::Kriging, new_x)
+    new_y = maximum(k.y)
+    add_point!(tmp_k, new_x, new_y)
+end
+
+# Mean Constant Liar
+function CLmean!(tmp_k::Kriging, k::Kriging, new_x)
+    new_y = mean(k.y)
+    add_point!(tmp_k, new_x, new_y)
+end
+
+# Kriging Believer
+function KB!(tmp_k::Kriging, k::Kriging, new_x)
+    new_y = k(new_x)
+    add_point!(tmp_k, new_x, new_y)
+end
+
+# Kriging Believer Upper Bound
+function KBUB!(tmp_k::Kriging, k::Kriging, new_x)
+    new_y = k(new_x) + 3 * std_error_at_point(k, new_x)
+    add_point!(tmp_k, new_x, new_y)
+end
+
+# Kriging Believer Lower Bound
+function KBLB!(tmp_k::Kriging, k::Kriging, new_x)
+    new_y = k(new_x) - 3 * std_error_at_point(k, new_x)
+    add_point!(tmp_k, new_x, new_y)
+end
