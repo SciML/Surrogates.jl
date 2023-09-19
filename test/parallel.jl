48d4601d330b44cf318f26c5318d397e25dc5ca6
Dreycen Foiles <foilesdreycen@gmail.com>
2023-09-19 17:49:55 -0500

    Renamed test file for parallelization to parallel.jl

--- test/parallel.jl
+++ test/parallel.jl
@@ -13,8 +13,10 @@ y = f.(x)
 
 # Test lengths of new_x and EI (1D)
 
+# TODO
+
 my_k = Kriging(x, y, lb, ub)
-new_x, eis = Ask(EI(), lb, ub, my_k, SobolSample(), 3, CLmean!)
+new_x, eis = potential_optimal_points(EI(), MeanConstantLiar(), lb, ub, my_k, SobolSample(), 3)
 
 @test length(new_x) == 3
 @test length(eis) == 3
@@ -22,7 +24,7 @@ new_x, eis = Ask(EI(), lb, ub, my_k, SobolSample(), 3, CLmean!)
 # Test lengths of new_x and SRBF (1D)
 
 my_surr = RadialBasis(x, y, lb, ub)
-new_x, eis = Ask(SRBF(), lb, ub, my_surr, SobolSample(), 3, CLmean!)
+new_x, eis = potential_optimal_points(SRBF(), MeanConstantLiar(), lb, ub, my_surr, SobolSample(), 3)
 @test length(new_x) == 3
 @test length(eis) == 3
 
@@ -37,7 +39,7 @@ y = f.(x)
 
 my_k = Kriging(x, y, lb, ub)
 
-new_x, eis = Ask(EI(), lb, ub, my_k, SobolSample(), 5, CLmean!)
+new_x, eis = potential_optimal_points(EI(), MeanConstantLiar(), lb, ub, my_k, SobolSample(), 5)
 
 @test length(new_x) == 5
 @test length(eis) == 5
@@ -47,13 +49,13 @@ new_x, eis = Ask(EI(), lb, ub, my_k, SobolSample(), 5, CLmean!)
 # Test lengths of new_x and SRBF (ND)
 
 my_surr = RadialBasis(x, y, lb, ub)
-new_x, eis = Ask(SRBF(), lb, ub, my_surr, SobolSample(), 5, CLmean!)
+new_x, eis = potential_optimal_points(SRBF(), MeanConstantLiar(), lb, ub, my_surr, SobolSample(), 5)
 
 @test length(new_x) == 5
 @test length(eis) == 5
 
 @test length(new_x[1]) == 3
 
-# # Check hyperparameter validation for Ask 
-@test_throws ArgumentError new_x, eis = Ask(EI(), lb, ub, my_k, SobolSample(), -1, CLmean!)
+# # Check hyperparameter validation for potential_optimal_points 
+@test_throws ArgumentError new_x, eis = potential_optimal_points(EI(), MeanConstantLiar(), lb, ub, my_k, SobolSample(), -1)
 
