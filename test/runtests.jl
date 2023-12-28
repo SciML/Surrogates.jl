8c3659c45c70e65e6ba5c381cd04f4b6b6174e63
Arno Strouwen <arno.strouwen@telenet.be>
2023-12-28 16:16:58 +0100

    Downgrade CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,6 @@ using Surrogates
 using Test
 using SafeTestsets
 using Pkg
-VERSION <= v"1.7" && Pkg.add(name = "Statistics", version = VERSION)
 
 function dev_subpkg(subpkg)
     subpkg_path = joinpath(dirname(@__DIR__), "lib", subpkg)
